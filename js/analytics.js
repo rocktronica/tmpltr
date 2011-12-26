001e0d9e5616af098cd0aefe62bde40515dbce69
Tommy <tommy@iamnotagoodartist.com>
2011-12-26 02:31:52 -0600

    ignore analytics

--- js/analytics.js
+++ js/analytics.js
@@ -1,1 +1,8 @@
-// this is where you'll put your analytics code, if you want.
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-9757644-11']);
+_gaq.push(['_trackPageview']);
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
